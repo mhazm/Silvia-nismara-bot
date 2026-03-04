a4d44dc074911674cad8fda67d2c71cfe39aa78f
mhaz <mhazmiee@gmail.com>
2026-03-05 03:00:51 +0700

    Check ongoing Job

--- src/commands/Jobs/myjob.js
+++ src/commands/Jobs/myjob.js
@@ -7,13 +7,25 @@ const {
 const DiscordBot = require('../../client/DiscordBot');
 const ApplicationCommand = require('../../structure/ApplicationCommand');
 const ActiveJob = require('../../models/activejob');
+const JobHistory = require('../../models/jobHistory');
 
 module.exports = new ApplicationCommand({
 	command: {
 		name: 'myjob',
-		description: 'Lihat job special contract yang sedang kamu jalankan',
+		description: 'Lihat job yang sedang kamu jalankan',
 		type: 1,
-		options: [],
+		options: [
+			{
+				name: 'game',
+				description: 'Pilih game untuk kontrak ini. 1 = ETS2, 2 = ATS',
+				choices: [
+					{ name: 'Euro Truck Simulator 2', value: 1 },
+					{ name: 'American Truck Simulator', value: 2 },
+				],
+				type: ApplicationCommandOptionType.Integer,
+				required: true,
+			},
+		],
 	},
 	options: {
 		allowedRoles: ['driver'],
@@ -27,28 +39,33 @@ module.exports = new ApplicationCommand({
 	run: async (client, interaction) => {
 		const guildId = interaction.guild.id;
 		const userId = interaction.user.id;
+		const gameId = interaction.options.getInteger('game');
 
-		const active = await ActiveJob.findOne({
+		const active = await JobHistory.findOne({
 			guildId,
 			driverId: userId,
-			active: true,
+			gameId: gameId,
+			jobStatus: 'ONGOING',
 		});
 		if (!active)
 			return interaction.reply(
-				'❌ Kamu tidak sedang menjalankan job special contract.',
+				'❌ Kamu tidak sedang menjalankan pekerjaan.',
 			);
 
 		const embed = new EmbedBuilder()
-			.setTitle('🚧 Special Contract Job Aktif')
+			.setTitle('🚧 Pekerjaan Aktif')
 			.setColor('Yellow')
 			.addFields(
-				{ name: 'Perusahaan', value: active.companyName, inline: true },
+				{ name: 'Game', value: `${active.game} (${active.gameMode})` },
+				{ name: 'Perusahaan', value: active.sourceCompany },
 				{
 					name: 'Rute',
-					value: `${active.source} → ${active.destination}`,
-					inline: true,
+					value: `${active.sourceCity} → ${active.destinationCity}`,
 				},
-				{ name: 'Kargo', value: active.cargo, inline: true },
+				{ name: 'Market', value: active.marketType },
+				{ name: 'Kargo', value: `${active.cargoName} (${active.cargoMass} t)`, inline: true },
+				{ name: 'Jarak', value: `${active.plannedDistanceKm} km`, inline: true },
+				{ name: 'Dimulai Pada', value: `<t:${Math.floor(active.startedAt.getTime() / 1000)}:F>`, inline: true },
 			)
 			.setFooter({ text: `Job ID: ${active.jobId}` })
 			.setTimestamp();
